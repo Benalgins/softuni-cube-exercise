5ddc00a4ecb565387b3ad1704169131352464b88
Benalgins <simeon.ivanew@gmail.com>
2024-05-22 19:45:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world')
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(PORT , () => console.log(`Listening on port ${PORT}`));
