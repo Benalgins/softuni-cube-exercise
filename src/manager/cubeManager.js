fc4774d28ff99cd232b5c69d255b031346a06aa9
Benalgins <simeon.ivanew@gmail.com>
2024-05-23 21:58:47 +0300

    Removed the hardcoded cubes

--- src/manager/cubeManager.js
+++ src/manager/cubeManager.js
@@ -1,23 +1,6 @@
 const cuid = require('cuid');
 
-let cubes = [
-  {
-    id: 'clwjiowd3000060vaegak81gt',
-    name: 'Rubik Cube',
-    description: 'this is easy cube',
-    imageUrl:
-      'https://m.media-amazon.com/images/I/81XkUCfu0mL._AC_UF894,1000_QL80_.jpg',
-    difficultyLevel: 1,
-  },
-  {
-    id: 'clwjiowd3dd400060vaegak81gt',
-    name: '8sided Cube',
-    description: 'This cube is difficult',
-    imageUrl:
-      'https://ae01.alicdn.com/kf/S7b1bab3d24af4517b95995dc415732366.jpg_640x640Q90.jpg_.webp',
-    difficultyLevel: 4,
-  },
-];
+let cubes = [];
 
 exports.getAll = (search, from, to) => {
   let result = cubes.slice();
