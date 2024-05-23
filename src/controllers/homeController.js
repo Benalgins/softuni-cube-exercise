440a21237564e0570a606d906eb6c6bdef55a2c6
Benalgins <simeon.ivanew@gmail.com>
2024-05-23 21:57:43 +0300

    Search bar saves values

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,7 +5,7 @@ const cubeManager = require('../manager/cubeManager');
 router.get('/', (req, res) => {
   const { search, from, to } = req.query;
   const cubes = cubeManager.getAll(search, from, to);
-  res.render('index', { cubes });
+  res.render('index', { cubes, search, from, to });
 });
 
 router.get('/about', (req, res) => {
